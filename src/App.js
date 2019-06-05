91e050719938e0e9e14b61f91861e69e44fefc26
Guilherme de Andrade <guilhermedeandrade@me.com>
2019-06-05 08:48:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Transition, animated } from "react-spring/renderprops";
 
-function App() {
+export default () => {
+  const [boxes, setBoxes] = useState(["hej", "tack", "smol"]);
+  const [foods, setFoods] = useState(["falafel", "kebab", "cheeseburger"]);
+
+  const removeByIndex = xs => idx => xs.filter(el => el !== xs[idx]);
+  const removeByValue = xs => value => xs.filter(el => el !== value);
+
+  const deleteBoxByIndex = removeByIndex(boxes);
+  const deleteFoodByValue = removeByValue(foods);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <TransitionGroup component="ul">
+          <h1>Transitions made with react-transition-group</h1>
+          {boxes.map((box, idx) => (
+            <CSSTransition timeout={500} classNames="fade" key={box}>
+              <li
+                key={box}
+                className="box"
+                onClick={() => setBoxes(deleteBoxByIndex(idx))}
+              >
+                {box}
+              </li>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </div>
+      <div>
+        <h1>Transitions made with react-spring</h1>
+        <ul>
+          <Transition
+            items={foods}
+            keys={item => item}
+            from={{ transform: "translate3d(0,-40px,0)", opacity: 0 }}
+            enter={{ transform: "translate3d(0,0px,0)", opacity: 1 }}
+            leave={{ transform: "translate3d(0,-40px,0)", opacity: 0 }}
+          >
+            {item => props => (
+              <animated.li
+                style={props}
+                className="box"
+                onClick={() => setFoods(deleteFoodByValue(item))}
+              >
+                {item}
+              </animated.li>
+            )}
+          </Transition>
+        </ul>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
